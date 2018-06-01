dfc92a0d9dbfff9a33508644172d3583103d4ca4
jonmitten <jonmitten@gmail.com>
2018-06-01 16:57:53 -0700

    adds Google Sheets API connectivity and tests

--- enlightener/enlightener.py
+++ enlightener/enlightener.py
@@ -5,32 +5,71 @@ Run this as follows:
 """
 import requests
 
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
 from requests.auth import HTTPBasicAuth
-from settings import USERNAME, PASSWORD, BASE_URL, RESOURCES
+from settings import USERNAME, PASSWORD, BASE_URL, RESOURCES, SPREADSHEET_ID
 
-username = USERNAME
-password = PASSWORD
-base_url = BASE_URL
-devicelist = RESOURCES['devicelist']
 
-class Enlightener():
-    """Main class."""
+basic_credentials = HTTPBasicAuth(USERNAME, PASSWORD)
+device_list = RESOURCES['devicelist']
 
-    def hello(self):
-        """Hello world."""
-        return 'hello world'
+# set up the Sheets API
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+store = file.Storage('credentials.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('../client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('sheets', 'v4', http=creds.authorize(Http()))
 
-    def connect_to_api(resource, authenticate=False, options=False):
-        """Simple Connect script to ensure API is responsive"""
-        auth = ''
-        if authenticate:
-            auth = HTTPBasicAuth(username, password)
 
-        if options:
-            # write options logic
-            pass
+# call the spreadsheets API
+RANGE_NAME = 'Sheet1!A1:C100'
 
-        url = ('{}/{}'.format(base_url, resource['resource_url']))
+def hello_sheets():
+    """Proves Sheets API connectivitiy."""
+    results = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME).execute()
 
+    values = results.get('values', [])
+    if not values:
+        print('No data found.')
+    else:
+        print('PTUnitID', 'Lightthreshold')
+        for row in values:
+            # print columns A and B, which correspond to indeces 0 and 1
+            print('{}, {}'.format(row[0], row[1]))
+
+    return values
+
+
+def hello():
+    """Hello world."""
+    return 'hello world'
+
+def build_device_list_url(**kwargs):
+    """Return a URL for fetching device list."""
+
+    return '{}/{}'.format(BASE_URL, device_list['resource_url'])
+
+def connect_to_api(authenticate=False, options=False):
+    """Simple Connect script to ensure API is responsive."""
+
+    auth = basic_credentials if authenticate else ''
+    url = build_device_list_url()
+    r = requests.get(url, auth=auth)
+
+    return r
+
+def get_device_list(device_list=None, **kwargs):
+    if device_list is None:
+        auth = basic_credentials
+        url = build_device_list_url()
         r = requests.get(url, auth=auth)
-        return r
+
+        return r.json()
+
+
+
--- tests/test_connections.py
+++ tests/test_connections.py
@@ -1,26 +1,40 @@
 """Test enlightener."""
 
 import sys
+import unittest
 sys.path.append('../enlightener')
 
-
-import unittest
-
-from enlightener import *
+from enlightener import get_device_list, hello, connect_to_api, hello_sheets
 
 
-class TestConnections(unittest.TestCase):
+class TestConnector(unittest.TestCase):
     """Test connections."""
 
     def test_hello(self):
         """Test import."""
-        self.assertEqual(Enlightener.hello(self), 'hello world')
+        self.assertEqual(hello(), 'hello world')
 
     def test_connect_to_api(self):
         """Test API connectivity."""
         # make sure a 200 is returned.
+        response = connect_to_api(True)
         self.assertEqual(
-            Enlightener.connect_to_api(devicelist, True).status_code, 200)
+            response.status_code, 200)
+
+    def test_get_device_list(self):
+        nose_test = False
+        resp = get_device_list()
+        mylist = resp['devices']
+        test_device_list = ['99000512002128', '99000512000619']
+        for device in test_device_list:
+            if device in mylist:
+                nose_test = True
+        self.assertEqual(nose_test, True)
+
+    def test_hello_sheets(self):
+        values = hello_sheets()
+
+        self.assertIn(['99000512002128', '1'], values)
 
 if __name__ == '__main__':
     unittest.main()
